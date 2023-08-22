2135e7e1d5f81bb286e0b29f9ffbbe2e39d6bf32
So Cool <127.0.0.1.pm@gmail.com>
2023-08-22 09:33:57 +0700

    admin->common-ui->store->products

---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Banner } from '@myorg/common-ui';
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <Banner text="Welcome to our admin app."/>
+      <div/>
+    </>
+  );
+}
+
+export default App;
--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,13 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { Banner } from '@myorg/common-ui';
+import {exampleProducts} from '@myorg/products';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="store" />
-    </div>
+    <>
+      <Banner text="Welcome to the store!"/>
+      <ul>
+        {exampleProducts.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong> Price: {product.price}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
